5ea8e6e6ba4420db8ee2db62088d3d0875ae4b10
David Mezler <david.mezler@outlook.com>
2024-02-18 01:07:03 +0100

    egui nice

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,66 @@
+mod search;
 use crate::search::search_pdf;
 
-mod search;
+use eframe::egui;
 
-fn main() {
-    let path = String::from("example");
-    search_pdf(path);
+fn main() -> Result<(), eframe::Error> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "pdf folder search",
+        options,
+        Box::new(|cc| {
+            // This gives us image support:
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+
+            Box::<MyApp>::default()
+        }),
+    )
+}
+
+struct MyApp {
+    name: String,
+    age: u32,
+    picked_path: String,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+            picked_path: "test".to_owned(),
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.vertical_centered(|ui| {
+                ui.heading("PDF Suche");
+            });
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Suche nach: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            if ui.button("Im Ordner").clicked() {
+                if let Some(path) = rfd::FileDialog::new().pick_folder() {
+                    self.picked_path = path.display().to_string();
+                }
+            }
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Increment").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!(
+                "Hello '{}', age {}, path: {}",
+                self.name, self.age, self.picked_path
+            ));
+        });
+    }
 }
--- src/search.rs
+++ src/search.rs
@@ -1,10 +1,25 @@
+use pdf_extract;
 use std::fs;
 
-pub fn search_pdf(path: String) -> String {
-    let paths = fs::read_dir(path).unwrap();
+pub fn search_pdf(path: String, search: String) -> Vec<String> {
+    let paths = fs::read_dir(path.clone());
+    let paths = match paths {
+        Ok(val) => val,
+        Err(_) => return vec![String::from("Fehler")], // TODO Fehler nicht so dumm bearbeiten.
+                                                       // Result zurück geben.
+    };
 
+    let mut result = vec![];
     for path in paths {
-        println!("Name: {}", path.unwrap().path().display());
+        let path = path.unwrap().path();
+        let out = pdf_extract::extract_text(&path).unwrap();
+        if out.contains(&search) {
+            let mut name = path.display().to_string();
+            name = String::from(name.split("/").last().unwrap());
+            name = format!("{}\n", name);
+            result.push(name);
+        }
+        // println!("{}", out);
     }
-    String::from("To be done")
+    result
 }
